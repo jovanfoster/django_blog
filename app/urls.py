ca2a53780de45400fa9992bab9c5c4aac4df1ce0
Jovan <jovanfoster9@gmail.com>
2020-08-26 07:09:57 -0400

    added create and update views

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),    
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post-detail'),
+    path('post/create/', views.create_post, name='post-create'),
+    path('post/<int:pk>/update/', views.update_post, name='post-update'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import DetailView
+
 from .models import *
+from .forms import *
+
 
 def index(request):
     posts = Post.objects.all()
     context = {'posts': posts}
 
     return render(request, 'app/index.html', context)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+
+    context = {'form': form}
+    
+    return render(request, 'app/post_create_form.html', context)
+
+
+def update_post(request, pk):
+    post = Post.objects.get(id=pk)
+    form = PostForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'form': form}
+    
+    return render(request, 'app/post_update_form.html', context)
 
 
 class PostDetail(DetailView):
